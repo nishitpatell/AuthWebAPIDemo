ae9a9d15dc105c59c623b7c7a600a3c2f1f5c9f3
Nishit Patel <pnishit238@gmail.com>
2025-09-18 11:01:59 +0530

    Add project files.

---
+++ AuthWebAPIDemo/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using AuthWebAPIDemo.Entities;
+using AuthWebAPIDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthWebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User user = new User();
+        [HttpPost("register")]
+        public ActionResult<User?> Register(UserDto request)
+        {
+            user.Username = request.Username;
+            user.PasswordHash = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User?> Login(UserDto request)
+        {
+
+        }
+    }
+}
---
+++ AuthWebAPIDemo/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthWebAPIDemo.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; // this won't be stored anywhere. it would be used to pass the pw entered by user to api
+    }
+}
